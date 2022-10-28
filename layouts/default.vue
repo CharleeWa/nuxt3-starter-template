9abc26587fb2d4d86eeea1ae1a1a34a4e2ada8b4
Charlie Wang <18888351756@163.com>
2022-10-28 17:20:37 +0800

    :bug: 修复 `pading` 溢出问题

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <div h-full w-full font-sans>
-    <div w-full bg-gray4:10 p3 text-center>
+    <div w-full box-border bg-gray4:10 p3 text-center>
       header
     </div>
     <div h-100 of-auto relative data-scroll>
       <slot />
     </div>
-    <div w-full bg-gray4:10 p3 text-center>
+    <div w-full box-border bg-gray4:10 p3 text-center>
       footer
     </div>
   </div>
