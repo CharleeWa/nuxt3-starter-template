0f92820b8e086474b65a907fee39c523d5fd78fb
CharleeWa <18888351756@163.com>
2022-11-29 16:31:22 +0800

    :sparkles: 错误处理页

---
+++ error.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps(['error'])
+
+const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+  <div class="mt-7 max-w-sm mx-auto text-center p-3 rounded-md bg-white shadow-md h-full">
+    <p class="mt-7 text-7xl font-bold">
+      {{ error.statusCode }}
+    </p>
+    <p class="mt-7 text-6xl">
+      Ooops.
+    </p>
+    <p class="mt-7">
+      {{ error.message }}
+    </p>
+    <button class="bg-[#12b488] text-white px-3 py-2 rounded-md text-sm my-7" @click="handleClearError">
+      Go Home...
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -6,7 +6,10 @@ const { id } = useRoute().params
 const uri = `https://fakestoreapi.com/products/${id}`
 
 // fetch the product
-const { data: product } = await useFetch(() => uri)
+const { data: product } = await useFetch(() => uri, { key: id })
+
+if (!product.value)
+  throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
 </script>
 
 <template>
